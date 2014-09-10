f02f065f108cdae65e170c9a1f67775d16356410
Matthew Henderson <matthew.james.henderson@gmail.com>
2014-09-10 09:38:55 +0100

    Some minor corrections to moore.py.

--- bin/moore.py
+++ bin/moore.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
+"""
+Usage:
+    $ geng -qc 4 | moore.py
+    C~
+"""
+
 from networkx import is_connected
 from networkx import parse_graph6, generate_graph6
 from igraph import Graph
 from sys import stdin
 
-def cage_order(d, k):
-    """Diameter is d. Degree is k."""
+def moore_order(d, k):
+    """
+    In a regular graph of degree k and diameter d the order is
+    at most moore_order(d, k).
+    """
     return 1 + k*sum([(k - 1)**i for i in range(d)])
 
 def moore(G):
+    """
+    Decide if G is a Moore graph or not, based on girth and diameter.
+    """
     K = max(G.degree().values())
     k = min(G.degree().values())
     if k != K or not is_connected(G):
@@ -19,22 +31,28 @@ def moore(G):
         return iG.girth() == 2*iG.diameter() + 1
 
 def moore2(G):
+    """
+    Decide if G is a Moore graph or not, based on order and diameter.
+    """
     K = max(G.degree().values())
     k = min(G.degree().values())
     if k != K or not is_connected(G):
         return False
     else:
         iG = Graph(edges = G.edges())
-        return G.order() == cage_order(iG.diameter(), K)
+        return G.order() == moore_order(iG.diameter(), K)
 
 def moore3(G):
+    """
+    Decide if G is a Moore graph or not, based on order and girth.
+    """
     K = max(G.degree().values())
     k = min(G.degree().values())
     if k != K or not is_connected(G):
         return False
     else:
         iG = Graph(edges = G.edges())
-        return G.order() == cage_order((iG.girth() - 1)/2, k)
+        return G.order() == moore_order((iG.girth() - 1)/2, k)
 
 if __name__=="__main__":
     for line in stdin.readlines():
