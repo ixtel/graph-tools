ecba400edd68be10ad14d18ebf57d723058115a0
Matthew Henderson <matthew.james.henderson@gmail.com>
2014-09-11 09:44:01 +0100

    Some improvements(?) to the Python-based Moore filter.

--- bin/moore.py
+++ bin/moore.py
@@ -6,15 +6,16 @@ Usage:
     C~
 """
 
-from networkx import is_connected
-from networkx import parse_graph6, generate_graph6
-from igraph import Graph
 from sys import stdin
+
+from networkx import is_connected
+from networkx import parse_graph6
+
+from igraph import Graph
 
 def moore_order(d, k):
     """
-    In a regular graph of degree k and diameter d the order is
-    at most moore_order(d, k).
+    The maximum number of vertices in a k-regular graph of diameter d.
     """
     return 1 + k*sum([(k - 1)**i for i in range(d)])
 
@@ -22,40 +23,37 @@ def moore(G):
     """
     Decide if G is a Moore graph or not, based on girth and diameter.
     """
-    K = max(G.degree().values())
-    k = min(G.degree().values())
-    if k != K or not is_connected(G):
-        return False
-    else:
-        iG = Graph(edges = G.edges())
-        return iG.girth() == 2*iG.diameter() + 1
+    degree = G.degree().values()
+    K = max(degree)
+    k = min(degree)
+    if is_connected(G) and k == K:
+      iG = Graph(edges = G.edges())
+      return iG.girth() == 2*iG.diameter() + 1
 
 def moore2(G):
     """
     Decide if G is a Moore graph or not, based on order and diameter.
     """
-    K = max(G.degree().values())
-    k = min(G.degree().values())
-    if k != K or not is_connected(G):
-        return False
-    else:
-        iG = Graph(edges = G.edges())
-        return G.order() == moore_order(iG.diameter(), K)
+    degree = G.degree().values()
+    K = max(degree)
+    k = min(degree)
+    if is_connected(G) and k == K:
+      iG = Graph(edges = G.edges())
+      return G.order() == moore_order(iG.diameter(), K)
 
 def moore3(G):
     """
     Decide if G is a Moore graph or not, based on order and girth.
     """
-    K = max(G.degree().values())
-    k = min(G.degree().values())
-    if k != K or not is_connected(G):
-        return False
-    else:
-        iG = Graph(edges = G.edges())
-        return G.order() == moore_order((iG.girth() - 1)/2, k)
+    degree = G.degree().values()
+    K = max(degree)
+    k = min(degree)
+    if is_connected(G) and k == K:
+      iG = Graph(edges = G.edges())
+      return G.order() == moore_order((iG.girth() - 1)/2, k)
 
 if __name__=="__main__":
     for line in stdin.readlines():
-        g = parse_graph6(line.rstrip())
-        if moore(g):
-            print generate_graph6(g, header = False)
+        stripped_line = line.rstrip()
+        g = parse_graph6(stripped_line)
+        if moore3(g): print stripped_line
