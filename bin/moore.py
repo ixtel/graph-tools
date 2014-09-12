3e7ea8e1c10a20d7942121f2690c03b5d8e901a4
Matthew Henderson <matthew.james.henderson@gmail.com>
2014-09-12 11:49:29 +0100

    Some improved function naming.

--- bin/moore.py
+++ bin/moore.py
@@ -19,31 +19,29 @@ def moore_order(d, k):
     """
     return 1 + k*sum([(k - 1)**i for i in range(d)])
 
-def moore1(G):
+def moore_gd(G):
     """
     Decide if G is a Moore graph or not, based on girth and diameter.
     """
     return G.girth() == 2*G.diameter() + 1
 
-def moore2(G):
+def moore_nd(G):
     """
     Decide if G is a Moore graph or not, based on order and diameter.
     """
-    K = max(G.degree())
-    return G.vcount() == moore_order(G.diameter(), K)
+    return G.vcount() == moore_order(G.diameter(), max(G.degree()))
 
-def moore3(G):
+def moore_gn(G):
     """
     Decide if iG is a Moore graph or not, based on order and girth.
     """
-    k = min(G.degree())
-    return G.vcount() == moore_order((G.girth() - 1)/2, k)
+    return G.vcount() == moore_order((G.girth() - 1)/2, min(G.degree()))
 
 if __name__=="__main__":
     for line in stdin.readlines():
         stripped_line = line.rstrip()
         g = parse_graph6(stripped_line)
         G = Graph(edges = g.edges())
-        moore = moore3
+        moore = moore_gn
         if moore(G):
             print stripped_line
