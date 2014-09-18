3535d378ca2e55de831615c7c1945f0754cffa5e
Matthew Henderson <matthew.james.henderson@gmail.com>
2014-09-18 13:59:37 +0100

    New program for greedy edge-colouring.

---
+++ bin/edge_colouring.py
@@ -0,0 +1,40 @@
+import sys
+
+def colour_edge(G, e, colour):
+    G.edge[e[0]][e[1]]['colour'] = colour
+
+def choice_mono(G, e, palette):
+    return palette[0]
+
+def used_at(G, u):
+    return set([G.edge[u][w].get('colour') for w in G.neighbors(u)])
+
+def choice_greedy(G, e, palette):
+    used_colours = used_at(G, e[0]).union(used_at(G, e[1]))
+    available_colours = set(palette).difference(used_colours)
+    return available_colours.pop()
+    
+def edge_colouring(G, choice = choice_greedy):
+    num_of_edges = len(G.edges())
+    num_coloured = 0
+    max_degree = max(G.degree().values())
+    palette = range(0, 2*max_degree)
+    for e in G.edges():
+        colour_edge(G, e, choice(G, e, palette))
+        num_coloured += 1
+        progress = 100.0*num_coloured/num_of_edges
+        sys.stdout.write("Progress: %d%%   \r" % progress)
+        sys.stdout.flush()
+    sys.stdout.write("\nDone!")
+    sys.stdout.flush()
+
+def is_proper_edge(G):
+    for u in G.node:
+        colours_at_u = []
+        for v in G.neighbors(u):
+            colours_at_u.append(G.edge[u][v]['colour'])
+        if len(set(colours_at_u)) != G.degree(u):
+            return False
+    return True
+
+
